0ca3bc7e6a440f3eaae5d6256fce4b8b835809b4
Shweetz <naruto92250@gmail.com>
2024-03-17 18:46:24 +0100

    day24

---
+++ 2023/day24/part1.py
@@ -0,0 +1,100 @@
+from time import time
+
+from dataclasses import dataclass
+import re
+
+@dataclass
+class Stone:
+	x : int
+	y : int
+	z : int
+	vx : int # named v in equations
+	vy : int # named w in equations
+	vz : int
+
+
+with open("input.txt", "r") as f:
+	lines = [l.strip() for l in f.readlines()]
+
+# script start
+t1 = time()
+stones = []
+min_x = 200000000000000
+max_x = 400000000000000
+total = 0
+
+for line in lines:
+	# put line's numbers in a list in different ways:
+	# 1. string replace
+	num_list = line.replace(",", "").replace("@", "").split()
+	# print(num_list)
+
+	# 2. re match
+	num_list = re.match("(.*), (.*), (.*) @ (.*), (.*), (.*)", line).groups()
+	# print(num_list)
+
+	# 3. re findall
+	num_list = re.findall(r'[-0-9]+', line)
+	# print(num_list)
+
+	x, y, z, vx, vy, vz = [int(e) for e in num_list if e]
+	print(x, y, z, vx, vy, vz)
+
+	stones.append(Stone(x, y, z, vx, vy, vz))
+
+for i, s1 in enumerate(stones):
+	for s2 in stones[i+1:]:
+		# find a, b time coefficients (in nanosecs) to find the intersection of lines of stones
+		# a/b positive means future encounter, negative means past encounter, 0 means present encounter
+
+		# s1.x + a * s1.vx = s2.x + b * s2.vx
+		# s1.y + a * s1.vy = s2.y + b * s2.vy
+		# <=> (s1.vx != 0 and s1.vy != 0)
+		# a = (s2.x + b * s2.vx - s1.x) / s1.vx
+		# a = (s2.y + b * s2.vy - s1.y) / s1.vy
+		# <=>
+		# (s2.x + b * s2.vx - s1.x) / s1.vx = (s2.y + b * s2.vy - s1.y) / s1.vy
+		# <=> (renaming)
+		# (x2   + bv2       - x1)   / v1    = (y2   + bw2       - y1)   / w1
+		# <=>
+		# bv2 = (y2v1 + bv1w2 - y1v1) / w1 + x1 - x2
+		# <=>
+		# bv2 - bv1w2/w1 = v1(y2 - y1)/w1 + x1 - x2
+		# <=>
+		# b((v2w1 - v1w2)/w1) = (v1(y2 - y1) + x1w1 - x2w1) / w1
+		# <=>
+		# b(v2w1 - v1w2) = v1(y2 - y1) + x1w1 - x2w1
+		# <=> (v2w1 - w2v1 != 0)
+		# b = (y2v1 - y1v1 + x1w1 - x2w1) / (v2w1 - v1w2)
+		
+		denominator = (s2.vx * s1.vy - s2.vy * s1.vx)
+		if denominator == 0:
+			# lines parallel, but maybe they are on the same trajectory
+			print("parallel")
+
+		else:
+			# plug values in the equations
+			b = (s2.y * s1.vx - s1.y * s1.vx + s1.x * s1.vy - s2.x * s1.vy) / denominator
+			a = (s2.x + b * s2.vx - s1.x) / s1.vx
+			# print(a, b)
+
+			x = s1.x + a * s1.vx
+			y = s1.y + a * s1.vy
+			# print(x, y)
+
+			# future_path = a >= 0 and b >= 0
+			if a >= 0 and b >= 0 and min_x <= x <= max_x and min_x <= y <= max_x:
+				total += 1
+
+		# print()
+
+print(total)
+print(f"Execution time: {(time() - t1):.3f}s")
+		
+# other way: find y = ax + b (where a = slope and b = offset) for both stones
+# a = s1.vy / s1.vx
+# b = y - ax = s1.y - s1.x * s1.vy / s1.vx
+# then when both lines intersect (y is equal): a1x + b1 = a2x + b2
+# <=>
+# x = (b2 - b1)/(a1 - a2)
+# y = a1x + b1 = a1 (b2 - b1)/(a1 - a2) + b1
---
+++ 2023/day24/part2.py
@@ -0,0 +1,135 @@
+from time import time
+
+from dataclasses import dataclass
+import re
+
+@dataclass
+class Stone:
+	x : int
+	y : int
+	z : int
+	vx : int
+	vy : int
+	vz : int
+
+
+def mat_calc(mat):
+	""" calculate matrix determinant """
+	if len(mat) == 1:
+		return mat[0][0]
+	sum = 0
+	for j in range(len(mat)):
+		sign = 1 if j % 2 == 0 else -1
+		sum += sign * mat[0][j] * mat_calc(mat_reduce(mat, j))
+	return sum
+
+def mat_reduce(mat, j):
+	""" remove row 0 and col j """
+	new_mat = []
+	for mat_line in mat[1:]:
+		new_mat.append(mat_line[:j] + mat_line[j+1:])
+	return new_mat
+
+# script start
+with open("input.txt", "r") as f:
+	lines = [l.strip() for l in f.readlines()]
+
+t1 = time()
+stones = []
+
+for line in lines:
+	# put line's numbers in a list in different ways:
+	# 1. string replace
+	num_list = line.replace(",", "").replace("@", "").split()
+	# print(num_list)
+
+	# 2. re match
+	num_list = re.match("(.*), (.*), (.*) @ (.*), (.*), (.*)", line).groups()
+	# print(num_list)
+
+	# 3. re findall
+	num_list = re.findall(r'[-0-9]+', line)
+	# print(num_list)
+
+	x, y, z, vx, vy, vz = [int(e) for e in num_list if e]
+	# print(x, y, z, vx, vy, vz)
+
+	stones.append(Stone(x, y, z, vx, vy, vz))
+
+# the rock (r) colling with a stone (s) at t0 means:
+# pxr + t0*vxr = pxs + t0*vxs
+# pyr + t0*vyr = pys + t0*vys
+# pzr + t0*vzr = pzs + t0*vzs
+#
+# we can remove t0 with some manipulation and combining x and y (z is ignored)
+# t0 = (pxs - pxr) / (vxr - vxs)
+# t0 = (pys - pyr) / (vyr - vys)
+# t0 = (pzs - pzr) / (vzr - vzs)
+#
+# (pxs - pxr) / (vxr - vxs) = (pys - pyr) / (vyr - vys)
+#
+# (pxs - pxr) * (vyr - vys) = (pys - pyr) * (vxr - vxs)
+#
+# pxs*vyr - pxs*vys - pxr*vyr + pxr*vys = pys*vxr - pys*vxs - pyr*vxr + pyr*vxs
+#
+# This is true for every hailstone n so we can use 2 hailstones and name them 0 and 1
+# px0*vyr - px0*vy0 - pxr*vyr + pxr*vy0 = py0*vxr - py0*vx0 - pyr*vxr + pyr*vx0
+# px1*vyr - px1*vy1 - pxr*vyr + pxr*vy1 = py1*vxr - py1*vx1 - pyr*vxr + pyr*vx1
+#
+# Substract both
+# px0*vyr - px0*vy0 + pxr*vy0 - px1*vyr + px1*vy1 - pxr*vy1 = py0*vxr - py0*vx0 + pyr*vx0 - py1*vxr + py1*vx1 - pyr*vx1
+#
+# pxr(vy0 - vy1) + pyr(vx1 - vx0) + vxr(py1 - py0) + vyr(px0 - px1) = px0*vy0 - py0*vx0 - px1*vy1 + py1*vx1
+#
+# Use stones to have 4 equations and use Cramer's rule (here, stone 0 everywhere, and stone 1 to 4 for 4 equations)
+
+A = [[], [], [], [], []] # A, A1, A2, A3, A4, each a 4x4 matrix
+s = stones[0]
+for i in range(4):
+	t = stones[i+1]
+
+	mat_line = [s.vy - t.vy, t.vx - s.vx, t.y - s.y, s.x - t.x]
+	A[0].append(mat_line)
+
+	Bi = s.x * s.vy - s.y * s.vx - t.x * t.vy + t.y * t.vx
+	for n in range(4):
+		A[n+1].append(list(mat_line)) # same line than A
+		A[n+1][i][n] = Bi # change one value with Bi
+
+A0 = mat_calc(A[0])
+A1 = mat_calc(A[1])
+A2 = mat_calc(A[2])
+A3 = mat_calc(A[3])
+A4 = mat_calc(A[4])
+
+pxr = A1 / A0
+pyr = A2 / A0
+vxr = A3 / A0
+vyr = A4 / A0
+
+# Repeat with x and z to find pzr and vzr (replace all y with z)
+
+# pxr(vz0 - vz1) + pzr(vx1 - vx0) + vxr(pz1 - pz0) + vzr(px0 - px1) = px0*vz0 - pz0*vx0 - px1*vz1 + pz1*vx1
+
+A = [[], [], [], [], []] # A, A1, A2, A3, A4, each a 4x4 matrix
+s = stones[0]
+for i in range(4):
+	t = stones[i+1]
+
+	mat_line = [s.vz - t.vz, t.vx - s.vx, t.z - s.z, s.x - t.x]
+	A[0].append(mat_line)
+
+	Bi = s.x * s.vz - s.z * s.vx - t.x * t.vz + t.z * t.vx
+	for n in range(4):
+		A[n+1].append(list(mat_line)) # same line than A
+		A[n+1][i][n] = Bi # change one value with Bi
+
+A0 = mat_calc(A[0])
+A2 = mat_calc(A[2])
+A4 = mat_calc(A[4])
+
+pzr = A2 / A0
+vzr = A4 / A0
+
+print(int(pxr + pyr + pzr))
+print(f"Execution time: {(time() - t1):.3f}s")
